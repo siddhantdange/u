6e923676d3e4837b06df4bbe3d4926f2573a8021
Sid Dange <dange.siddhant@gmail.com>
2019-06-07 23:04:42 -0700

    complete problem 1: driver metrics

--- src/analyze/Analysis.py
+++ src/analyze/Analysis.py
@@ -2,3 +2,6 @@
 class Analysis:
     def __init__(self, data_csvs):
         self.data_csvs = data_csvs
+
+    def get_driver_metrics(self):
+        pass
--- src/analyze/DriverMetrics.py
+++ src/analyze/DriverMetrics.py
@@ -7,8 +7,18 @@ from Analysis import Analysis
 
 class DriverMetrics(Analysis):
     def get_driver_metrics(self):
-        print(self.avg_trips_per_hour_per_driver())
-        print('done!')
+        message = """
+        Driver Metrics:
+        ------------------------------------------------
+        Average trips length: {avg_trip_length}
+
+        Average trips per hour over all drivers: {avg_trips_per_hour}
+
+        Average driver utilization: {avg_driver_util}
+
+        """.format(avg_trip_length=str(self.avg_trip_lengths()), avg_trips_per_hour=self.avg_trips_per_hour_per_driver(), avg_driver_util=self.get_avg_driver_util())
+
+        print(message)
 
     def avg_trip_lengths(self):
         reader = RideReader(self.data_csvs[0])
@@ -37,7 +47,6 @@ class DriverMetrics(Analysis):
             if not ymd in driver_to_hours[ride.driver][ride.pickup_datetime.hour]['days']:
                 driver_to_hours[ride.driver][ride.pickup_datetime.hour]['days'].add(ymd)
 
-
         hour_averages = {}
         for hour in list(range(24)):
             hour_averages[hour] = []
@@ -52,4 +61,39 @@ class DriverMetrics(Analysis):
             hour_average[hour] = sum(hour_averages[hour])/len(hour_averages[hour])
 
         return hour_average
+
+    # util = all_trip_durations/(last_start_day - first_start_day).to_s
+    def get_avg_driver_util(self):
+        reader = RideReader(self.data_csvs[0])
+
+        driver_trip_durations = {}
+        for i in range(reader.get_num_rows()):
+            ride = reader.get_ride_at_index(i)
+            if ride.driver not in driver_trip_durations:
+                driver_trip_durations[ride.driver] = {
+                    'total_d': 0,
+                    'first': ride.pickup_datetime,
+                    'last': ride.pickup_datetime
+                }
+
+            driver_trip_durations[ride.driver]['total_d'] += ride.trip_time_in_secs
+            if ride.pickup_datetime < driver_trip_durations[ride.driver]['first']:
+                driver_trip_durations[ride.driver]['first'] = ride.pickup_datetime
+
+            if ride.pickup_datetime > driver_trip_durations[ride.driver]['last']:
+                driver_trip_durations[ride.driver]['last'] = ride.pickup_datetime
+
+        total_util = 0
+        num_valid_drivers = 0
+
+        utils = []
+        for driver in driver_trip_durations.keys():
+            duration_data = driver_trip_durations[driver]
+
+            if driver_trip_durations[driver]['first'] == driver_trip_durations[driver]['last']:
+                continue
+
+            utils.append(float(duration_data['total_d'])/(duration_data['last'] - duration_data['first']).total_seconds())
+
+        return np.median(utils)
 
